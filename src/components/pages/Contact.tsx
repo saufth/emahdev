818010e7ec6c25e364ee606ce6e2358a7493af32
saufth <saufth@gmail.com>
2023-01-27 23:41:41 -0600

    Contact form created

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,5 +1,8 @@
+// React
+import { FormEvent, useRef } from 'react'
+// Services
+import emailjs from '@emailjs/browser'
 // Styles
-import { useRef } from 'react'
 import styles from '../../styles/pages/Contact.module.css'
 
 /**
@@ -8,10 +11,26 @@ import styles from '../../styles/pages/Contact.module.css'
 */
 const Contact = () => {
   const formRef = useRef<HTMLFormElement>(null)
+  const sendEmail = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const node = formRef.current
+    if (node) {
+      emailjs.sendForm(
+        String(process.env.NEXT_PUBLIC_EMAIL_SERVICE_KEY),
+        String(process.env.NEXT_PUBLIC_EMAIL_TEMPLATE_KEY),
+        formRef.current,
+        String(process.env.NEXT_PUBLIC_EMAIL_PUBLIC_KEY)
+      ).then(() => {
+        alert('Hemos recibido tu mensaje')
+      }, () => {
+        alert('Lo sentimos, hubo un problema al intentar enviar el mensaje')
+      })
+    }
+  }
 
   return (
     <section className={styles.section}>
-      <form className={styles.form} ref={formRef}>
+      <form className={styles.form} onSubmit={sendEmail} ref={formRef}>
         <div>
           <input
             className={styles.input}
